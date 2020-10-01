e69abbbe13a54acbc1dbee84f860dec884d9249d
SOFIA <rusofiaru@gmail.com>
2020-10-01 18:49:37 +0300

    part 3 (c)

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-﻿const express = require('express')
+﻿require('dotenv').config()
+const express = require('express')
+const handlers = require('./handlers')
 const morgan = require('morgan')
+const Person = require('./models/person')
 morgan.token('body', (req, res) => {
-    if (req.method==='POST') {
+    if (req.method === 'POST') {
         const body = JSON.stringify(req.body);
         return body;
     }
@@ -11,67 +14,33 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    },
-    {
-        "name": "re",
-        "number": "44",
-        "id": 5
-    },
-    {
-        "name": "f",
-        "number": "4",
-        "id": 6
-    },
-    {
-        "name": "ffffff",
-        "number": "3333",
-        "id": 7
-    }
-];
-
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+    Person.find({}).then(result => {
+        response.json(result)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(p => p.id === id);
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-    response.json(person);
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
@@ -80,35 +49,45 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const existingPerson = persons.find(p => p.name === body.name);
-    if (existingPerson) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    });
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+})
+
+app.put('/api/persons/:id', (request, response, next)  => {
+    const body = request.body
 
     const person = {
         name: body.name,
         number: body.number,
-        id: generateId(),
     }
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/info', (request, response) => {
     const receivedDate = new Date();
-    const infoMessage = `Phonebook has info for ${persons.length} people \n \n ${receivedDate}`;
-    response.send(infoMessage);
+    const personsLength = Person.find({})
+        .then(result => {
+            const infoMessage = `Phonebook has info for ${result.length} people \n \n ${receivedDate}`;
+            response.send(infoMessage);
+        })
 })
 
-const generateId = () => {
-    return Math.floor(Math.random() * Math.floor(1000000));
-}
+app.use(handlers.unknownEndpoint)
 
-const PORT = process.env.PORT || 3001;
+app.use(handlers.errorHandler)
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
